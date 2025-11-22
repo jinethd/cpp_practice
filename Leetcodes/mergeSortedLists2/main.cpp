bf58991e405b46f0043c42c3f74dd7fd4a8947f0
Jineth <jinethd@gmail.com>
2025-11-22 23:19:10 +0000

    working

--- Leetcodes/mergeSortedLists2/main.cpp
+++ Leetcodes/mergeSortedLists2/main.cpp
@@ -27,9 +27,15 @@ public:
 
         int i = m-1;
         int j = n-1;
-        int m = n+m-1;
+        int k = n+m-1;
 
-        
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
+
+        while(j>=0) nums1[k--]=nums2[j--];
+
     }
 };
 
