7821eb70cf7c8b582348216944f71da637357f93
Jineth <jinethd@gmail.com>
2025-11-29 16:06:31 +0000

    sol v1

--- Leetcodes/numberOfIslands/main.cpp
+++ Leetcodes/numberOfIslands/main.cpp
@@ -5,24 +5,55 @@
 class Solution {
 public:
     int numIslands(std::vector<std::vector<char>>& grid) {
-        
-        return 0;
+
+        int counter =0;
+
+        for (int i=0; i<grid.size(); i++) {
+            for (int j=0; j < grid[0].size();j++) {
+                if (grid[i][j] == '1'){
+                    dfs(grid,i,j);
+                    ++counter;;
+                }
+            }
+        } 
+
+        for (int i=0; i<grid.size(); i++) {
+            std::cout<<"{ ";
+            for (int j=0; j < grid[0].size();j++) {
+                std::cout<< grid[i][j];
+                if (j != grid[0].size()-1) std::cout<<", ";
+            }
+            std::cout<<" }"<<std::endl;
+        }        
+        return counter;
     }
 
 private:
     void dfs(std::vector<std::vector<char>>& grid, int r, int c) {
-        
+        if (r<0 || r>=grid.size() ||
+            c >= grid[0].size() || c<0 || grid[r][c] != '1' ) return;
+
+        grid[r][c] = 'X';
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
+        dfs(grid, r+1, c);
     }
 };
 
 int main() {
     Solution sol;
 
+    // std::vector<std::vector<char>> grid = {
+    //     {'1','1','0','0','0'},
+    //     {'1','1','0','0','0'},
+    //     {'0','0','1','0','0'},
+    //     {'0','0','0','1','1'}
+    // };
+
     std::vector<std::vector<char>> grid = {
-        {'1','1','0','0','0'},
-        {'1','1','0','0','0'},
-        {'0','0','1','0','0'},
-        {'0','0','0','1','1'}
+        {'0','1','0'},
+        {'1','1','1'},
+        {'0','1','0'}
     };
 
     std::cout << sol.numIslands(grid) << "\n";
