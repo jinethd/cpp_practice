2724df93ccfac6424d5ea2ca42a727c77b513ac5
Jineth <jinethd@gmail.com>
2025-12-07 23:13:17 +0000

    solution to new two pointer problem

---
+++ Leetcodes/containerWithMostWater/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        
+
+        int left = 0;
+        int right = height.size() - 1;
+        int best = 0;
+
+        while (left < right) {
+
+            int area = (right-left) * std::min(height[left],height[right]);
+
+            best = std::max(area,best);
+
+            if (height[left]<=height[right]){
+                left++;
+            } else{
+                right--;
+            }
+
+        }
+
+        return best;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    std::vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    int result = sol.maxArea(height);
+
+    std::cout << "Max water area: " << result << "\n";
+
+    return 0;
+}
