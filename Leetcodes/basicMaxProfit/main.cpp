264b4e53e821fa67eae8d5dd14c052860523c9d6
Jineth <jinethd@gmail.com>
2025-12-14 18:38:02 +0000

    my solution to a basic max price problem

---
+++ Leetcodes/basicMaxProfit/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices){
+        int max = 0;
+        int lowest = prices[0];
+
+        for (int num : prices){
+
+            if (num<lowest) lowest = num;
+
+            if ((num-lowest)>max) max = num-lowest;
+        }
+
+        return max;
+    };
+};
+
+int main() {
+    Solution sol;
+    std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    std::cout<<sol.maxProfit(prices)<<std::endl;
+    return 0;
+}
