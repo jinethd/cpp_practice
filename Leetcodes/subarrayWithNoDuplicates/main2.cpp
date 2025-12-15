bfaa134062751aad54e9931d081627d77311fb96
Jineth <jinethd@gmail.com>
2025-12-15 12:54:26 +0000

    using 'last seen at index' array

---
+++ Leetcodes/subarrayWithNoDuplicates/main2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+class Solution{
+public:
+    int subarrayWithNoDupes(const std::string& s){
+
+        if(s.empty()) return 0;
+        
+        size_t bestLength = 0, left = 0;
+        std::unordered_map<char, int> lastSeen;
+
+        for(size_t right=0;right<s.size();right++){
+            char c = s[right];
+
+            if (lastSeen.count(c) && lastSeen[c] > left){
+                left = lastSeen[c] + 1;
+            }
+
+            lastSeen[c] = right;
+
+            bestLength = std::max(bestLength, right-left+1);
+            
+        }
+
+        return bestLength;
+
+    };
+};
+
+int main(){
+
+    Solution sol;
+
+    std::string s = "abba";
+
+    std::cout<<sol.subarrayWithNoDupes(s)<<'\n';
+
+    return 0;
+}
